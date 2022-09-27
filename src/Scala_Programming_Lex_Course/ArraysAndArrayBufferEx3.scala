35dd992e787253bafd4630b5eeddd281785c90fa
SuyogBadjate <suyog.badjate@gmail.com>
2022-09-27 16:28:10 +0530

    new commit

--- src/Scala_Programming_Lex_Course/ArraysAndArrayBufferEx3.scala
+++ src/Scala_Programming_Lex_Course/ArraysAndArrayBufferEx3.scala
@@ -5,12 +5,12 @@ package Scala_Programming_Lex_Course
 object ArraysAndArrayBufferEx3 {
       def main(args:Array[String]): Unit = {
         val randInt=scala.util.Random.nextInt(11)
-        var wantToPlay=true
-        while(wantToPlay){
+        var wantToPlay1=true
+        while(wantToPlay1){
          val input=scala.io.StdIn.readInt()
          
          if(input==randInt){  
-           wantToPlay=false
+           wantToPlay1=false
            println("Correct right number")
          }else if(input>randInt){ 
            println("Lower Number")
